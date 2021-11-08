d2f6615d5aa645d014fe8dde631d9b6345315286
Fredenbxy <2931162910@qq.com>
2021-11-08 21:53:27 +0800

    commit push

--- src/main/java/com/acca/demo/DemoApplication.java
+++ src/main/java/com/acca/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("git for master code change222   bxy20211108");
 		System.out.println("git for master code change pull 2132");
-		System.out.println("git for hot-fix code change");
+		System.out.println("git for hot-fix code change push aaaaaaa 2143");
 		System.out.println("git for hot-fix code change hotfix");
 		SpringApplication.run(DemoApplication.class, args);
 	}
