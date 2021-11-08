bd14517e667b2a28019ef6fe55d9c72f902a89bc
Fredenbxy <55916578+Fredenbxy@users.noreply.github.com>
2021-11-08 21:04:10 +0800

    Update DemoApplication.java

--- src/main/java/com/acca/demo/DemoApplication.java
+++ src/main/java/com/acca/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("git for master code change222");
+		System.out.println("git for master code change222   bxy20211108");
 		System.out.println("git for master code change");
 		System.out.println("git for hot-fix code change");
 		System.out.println("git for hot-fix code change hotfix");
